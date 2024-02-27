8e35e5de985f5c231f07b972ed9d88d009d6fbda
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-02-27 10:52:28 +0700

    setup env

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>TEST FONT NUNITO</h1>
+      <p>Go to Dashboard</p>
     </main>
   );
 }
