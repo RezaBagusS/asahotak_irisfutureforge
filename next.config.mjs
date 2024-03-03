607deeada63a876790e90eb0729e89d7af20169a
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-03-03 22:41:45 +0700

    setup nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
     compiler: {
         styledComponents: true,
-    }
+    },
+    env: {
+        DB_HOST: process.env.DB_HOST,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_NAME: process.env.DB_NAME,
+        SECRET_KEY: process.env.SECRET_KEY,
+    },
 };
 
 export default nextConfig;
